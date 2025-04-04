6f4c86f6628d575c007fe4128f4ed4452f7eac28
jihed <jihedoueslati4@gmail.com>
2025-04-04 14:03:48 +0200

    Updated API Gateway configuration

---
+++ src/main/java/com/esprit/ms/apigateway4sa11/ApiGateway4Sa11Application.java
@@ -0,0 +1,31 @@
+package com.esprit.ms.apigateway4sa11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGateway4Sa11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateway4Sa11Application.class, args);
+	}
+
+	@Bean
+	public RouteLocator getRouteApiGateway(RouteLocatorBuilder builder)
+	{
+		return builder.routes()
+
+				.route("vol-route", r -> r
+						.path("/api/flights/**")
+						.uri("lb://Vol"))  // Match the service name
+				.build();
+
+
+
+	}
+}
